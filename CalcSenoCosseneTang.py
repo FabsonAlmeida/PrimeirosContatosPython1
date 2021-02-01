cc7c77af3723b517ffdad20174d3f23e64df0f1b
Fabson <fabson_sa@hotmail.com>
2021-02-01 11:05:17 -0300

    Atualização do git fev.1

--- CalcSenoCosseneTang.py
+++ CalcSenoCosseneTang.py
@@ -1,8 +1,8 @@
-import math
+import mat
 
 print('Coloque um valor de ângulo para ter seno, cosseno e tangente')
 angulo=float(input('Digite o valor do ângulo'))
-cosseno=math.cos(angulo)
-seno=math.sin(angulo)
-tangente=math.tan(angulo)
+cosseno= mat.cos(angulo)
+seno=mat.sin(angulo)
+tangente=mat.tan(angulo)
 print('O angulo digitado foi {}, o seno deste ângulo é {}, o cosseno é {} e a tangente é {}'.format(cosseno, seno, tangente))
